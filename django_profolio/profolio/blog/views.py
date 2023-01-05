78eeb9138125e6952964848636bbdc642ebd3d32
alldaygzchen <gonzalochen66@gmail.com>
2023-01-05 23:44:17 +0800

    django leetcode profolio ongoing

--- django_profolio/profolio/blog/views.py
+++ django_profolio/profolio/blog/views.py
@@ -1,108 +1,35 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "jiujitsu-addiction",
-        "image": "jiujitsu.jpg",
-        "author": "gz",
-        "date": date(2021, 7, 21),
-        "title": "Jiujitsu is fun!",
-        "excerpt": "Have you ever try it",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-addiction",
-        "image": "coding.jpg",
-        "author": "gz",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours coding",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "deadlift-addiction",
-        "image": "deadlift.jpg",
-        "author": "gz",
-        "date": date(2020, 8, 5),
-        "title": "Deadlift is the best",
-        "excerpt": "Deadlift is amazing!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "manga-addiction",
-        "image": "manga.jpg",
-        "author": "gz",
-        "date": date(2020, 8, 5),
-        "title": "manga is cool",
-        "excerpt": "Addiction in manga",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 
-def get_date(post):
-  return post['date']
 
+# def get_date(post):
+#   return post['date']
+# sorted_posts = sorted(all_posts, key=get_date)
 # Create your views here.
-
+# identified_post = next(post for post in all_posts if post['slug'] == slug)
+# return render(request, "blog/post-detail.html", {
+#   "post": identified_post
+# })
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-      "posts": latest_posts
-    })
+  latest_posts = Post.objects.all().order_by("-date")[:6]
+  return render(request, "blog/index.html", {
+    "posts": latest_posts
+  })
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html", {
-      "all_posts": all_posts
-    })
+  all_posts = Post.objects.all().order_by("-date")
+  return render(request, "blog/all-posts.html", {
+    "all_posts": all_posts
+  })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html", {
-      "post": identified_post
-    })
+  identified_post = get_object_or_404(Post, slug=slug)
+  return render(request, "blog/post-detail.html", {
+    "post": identified_post,
+    "post_tags": identified_post.tags.all()
+  })
