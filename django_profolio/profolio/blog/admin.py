94246a13ebb7e71a9dca05ce6bfd1ef31e3fa883
alldaygzchen <gonzalochen66@gmail.com>
2023-01-20 00:06:05 +0800

    profolio not done

--- django_profolio/profolio/blog/admin.py
+++ django_profolio/profolio/blog/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 
-from .models import Post, Author, Tag
+from .models import Post, Author, Tag, Comment
 
 # Register your models here
 
@@ -11,7 +11,12 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author",)
     prepopulated_fields = {"slug": ("title",)}
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment,CommentAdmin)
